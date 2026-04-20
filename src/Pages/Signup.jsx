6834848911d0b90170fb49eebbea201c76883a1e
SparklingStar212 <deborahalabi49@gmail.com>
2026-04-20 08:16:03 +0100

    feat: update Signup component with improved form structure and validation, change API endpoint

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Formik, useFormik } from 'formik'
-import * as yup from 'yup'
+import { useFormik } from 'formik'
 import axios from 'axios'
+import * as yup from 'yup'
 import { useNavigate } from 'react-router-dom'
 
 
 const Signup = () => {
   const navigate = useNavigate()
+
   const formik = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
       email: '',
-      password: '',
-      cPassword: ''
+      password: ''
     },
     onSubmit: (values) => {
       console.log(values)
-      axios.post('https://mongodb-bqfr.onrender.com', values)
+      axios.post('http://localhost:3040/user/register', values)
         .then(response => {
           alert('Form submitted successfully')
           navigate('/login')
@@ -29,50 +29,55 @@ const Signup = () => {
     validationSchema: yup.object({
       firstName: yup.string().required('First name is required'),
       lastName: yup.string().required('Last name is required'),
-      email: yup.string().email('Invalid email address').required('Email is required'),
-      password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-      cPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required')
+      email: yup.string().email('Invalid email format').required('Email is required'),
+      password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
     })
   })
+
+
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <p>Register </p>
-      <p>Signup now and get full access to our app. </p>
+    <>
+      <h1>Signup Page</h1>
+      <form>
+        <input className="d-block mb-1"
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.firstName ? <p className="text-danger fs-6">{formik.errors.firstName}</p> : ""}
 
+        <input className="d-block mb-1"
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.lastName ? <p className="text-danger fs-6">{formik.errors.lastName}</p> : ""}
 
+        <input className="d-block mb-1"
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email ? <p className="text-danger fs-6">{formik.errors.email}</p> : ""}
 
-      <label className='d-block mb-1' htmlFor='firstName'>Firstname</label>
-      <input required type="text" name='firstName' value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-      {formik.touched.firstName ? <p className='text-danger'>{formik.errors.firstName}</p> : ""}
+        <input className="d-block mb-1"
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password && formik.errors.password ? <p className="text-danger fs-6">{formik.errors.password}</p> : ""}
 
-
-
-      <label className='d-block mb-1' htmlFor='lastName'>Lastname</label>
-      <input required type="text" name='lastName' value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-      {formik.touched.lastName ? <p className='text-danger'>{formik.errors.lastName}</p> : ""}
-
-
-      <label className='d-block mb-1' htmlFor='email'>Email</label>
-      <input required type="email" name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-      {formik.touched.email ? <p className='text-danger'>{formik.errors.email}</p> : ""}
-
-
-      <label className='d-block mb-1' htmlFor='password'>Password</label>
-      <input required type="password" name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-      {formik.touched.password ? <p className='text-danger'>{formik.errors.password}</p> : ""}
-
-
-      <label className='d-block mb-1' htmlFor='cPassword'>Confirm Password</label>
-      <input required type="password" name='cPassword' value={formik.values.cPassword} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-      {formik.touched.cPassword ? <p className='text-danger'>{formik.errors.cPassword}</p> : ""}
-
-      <br />
-
-
-      <button className="submit" type="submit">Submit</button>
-      <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-    </form>
-
+        <button type="submit" onClick={formik.handleSubmit}>Sign Up</button>
+      </form>
+    </>
   )
 }
 
